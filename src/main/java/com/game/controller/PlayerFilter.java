b2cc76cdb8a0d17b1a37cf3b949f2a5cfc328f04
VladimirSafronov <safron5000@mail.ru>
2022-09-16 14:36:42 +0300

    Создание игрока и фильтрация списка игроков
    * реализована логика создания нового игрока
    * реализована логика фильтрации по полям name и title

---
+++ src/main/java/com/game/controller/PlayerFilter.java
@@ -0,0 +1,22 @@
+package com.game.controller;
+
+public class PlayerFilter {
+  private String name;
+  private String title;
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
