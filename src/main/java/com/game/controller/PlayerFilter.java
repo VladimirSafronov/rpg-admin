ba314ccb5539ce334b406168720638215415349d
VladimirSafronov <safron5000@mail.ru>
2022-09-19 20:20:24 +0300

    Добавлен фильтр по бану игроков

--- src/main/java/com/game/controller/PlayerFilter.java
+++ src/main/java/com/game/controller/PlayerFilter.java
@@ -3,8 +3,26 @@ package com.game.controller;
 public class PlayerFilter {
   private String name;
   private String title;
+  private Boolean banned;
+//  private Date birthday;
+//
+//    public Date getBirthday() {
+//        return birthday;
+//    }
+//
+//    public void setBirthday(Date birthday) {
+//        this.birthday = birthday;
+//    }
 
-  public String getTitle() {
+    public Boolean isBanned() {
+        return banned;
+    }
+
+    public void setBanned(Boolean banned) {
+        this.banned = banned;
+    }
+
+    public String getTitle() {
     return title;
   }
 
